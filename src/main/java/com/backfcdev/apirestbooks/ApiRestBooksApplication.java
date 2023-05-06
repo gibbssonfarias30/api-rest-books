22220e09341385e859ecccee90a4e5e1049c9433
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-05-06 15:56:34 -0500

    added the project to github

---
+++ src/main/java/com/backfcdev/apirestbooks/ApiRestBooksApplication.java
@@ -0,0 +1,13 @@
+package com.backfcdev.apirestbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestBooksApplication.class, args);
+	}
+
+}
